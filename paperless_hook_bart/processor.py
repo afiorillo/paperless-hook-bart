398653da430339d84393cac92365d312c8172394
Andrew Fiorillo <andrewmfiorillo@gmail.com>
2023-09-14 22:26:25 +0200

    And now the post_consume_script does what it's supposed to: get a document from Paperless, get the vectors, and store them.

--- paperless_hook_bart/processor.py
+++ paperless_hook_bart/processor.py
@@ -1,7 +1,12 @@
+from typing import NamedTuple
+
 from requests.exceptions import RequestException
 from pydantic import ValidationError
 
+from paperless_hook_bart.embeddings import BartEmbedder
 from paperless_hook_bart.paperless_client import PaperlessClient
+from paperless_hook_bart.settings import PaperlessServerSettings
+from paperless_hook_bart.vector_store import DiskVectorStore
 
 
 class IngestionError(Exception):
@@ -11,21 +16,44 @@ class IngestionError(Exception):
 class UnreadableDocument(IngestionError):
     """The document did not contain any contents, so it can't be embedded."""
 
+class IngestionResult(NamedTuple):
+    ingested_documents: int
+    ingested_vectors: int
 
-def ingest_document(client: PaperlessClient, document_id: int):
-    """
-    Fetches data for an already consumed document in Paperless and stores its embeddings
-    in the vector store.
-    """
-    try:
-        doc = client.get_document(hook_settings.document_id)
-    except RequestException as err:
-        raise IngestionError("unable to fetch from paperless server") from err
-    except ValidationError as err:
-        raise IngestionError("unable to parse response") from err
+class Processor:
 
-    contents = doc.content
-    if not contents:
-        raise UnreadableDocument(f"document is empty? {doc.id}")
+    def __init__(self, paperless_settings: PaperlessServerSettings):
+        self.client = PaperlessClient(
+            base_url=str(paperless_settings.paperless_base_url),
+            token=paperless_settings.paperless_token,
+        )
+        self.store = DiskVectorStore('vectorstore.parquet')
+        # get this lazily, since it occupies a lot of memory
+        self._embedder = None
 
-    # TODO open the vector store, chunk the contents, embed them, store them.
+    @property
+    def embedder(self) -> BartEmbedder:
+        if self._embedder is None:
+            self._embedder = BartEmbedder()
+        return self._embedder
+
+    def ingest_document(self, document_id: int) -> IngestionResult:
+        """
+        Fetches data for an already consumed document in Paperless and stores its embeddings
+        in the vector store.
+        """
+        try:
+            doc = self.client.get_document(document_id)
+        except RequestException as err:
+            raise IngestionError("unable to fetch from paperless server") from err
+        except ValidationError as err:
+            raise IngestionError("unable to parse response") from err
+
+        contents = doc.content
+        if not contents:
+            raise UnreadableDocument(f"document is empty? {doc.id}")
+
+        vectors = self.embedder.get_embeddings(contents)
+        for vec in vectors:
+            self.store.store(vec, document_id=doc.id)
+        return IngestionResult(1, len(vectors))
